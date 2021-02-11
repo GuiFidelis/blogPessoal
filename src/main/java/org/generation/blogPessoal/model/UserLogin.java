0a98ceac41ac41a71a07b1254e6aac80b7bea60c
GuiFidelis <68570304+GuiFidelis@users.noreply.github.com>
2021-02-11 05:42:39 -0300

    back-end atualizado

--- src/main/java/org/generation/blogPessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -2,6 +2,8 @@ package org.generation.blogPessoal.model;
 
 public class UserLogin {
 	
+	private long id;
+	
 	private String nome;
 
 	private String usuario;
@@ -10,8 +12,20 @@ public class UserLogin {
 	
 	private String token;
 
+	private String foto;
+
+	private String tipo;
+
 	public String getNome() {
 		return nome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public void setNome(String nome) {
@@ -41,7 +55,22 @@ public class UserLogin {
 	public void setToken(String token) {
 		this.token = token;
 	}
-	
-	
 
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	
 }
